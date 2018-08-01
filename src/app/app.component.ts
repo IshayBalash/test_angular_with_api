6bf3270c2a5b0bdca0e518f0c4d19f6593460ed5
Ishay Balash <35499532+IshayBalash@users.noreply.github.com>
2018-08-01 17:28:23 +0300

    basic_api_angular_project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,78 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {Product} from "../app/shared/models/product.modle"
 import { ProductService } from './shared/services/Productservice.service';
 import { ProductListclass } from './shared/models/productList.modle';
+import { timeout } from 'q';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   
-  test:number=5;
+ 
   productList:ProductListclass=new ProductListclass()
+  ShowProductList:boolean;
+
+  ResponsMassage:string="AAA";
+
+  DidPostWork:string="show post result";
+
+  DidPutWork:string="show put result"
+
+
+
+  getAllProducts():void{
+  this.myProductService.InitProducts()
+  this.ShowProductList=true;
+  console.log(this.productList.productsArry)
+  
+  }
+
+  getSpesificPeoduct(param:string):void{
+    this.myProductService.SetSpecificProduct(param)
+    console.log(this.productList.singleProduct)   
+  }
+
+
+   Productname2:string="Sprit"
+  DeleteProduct(productname:string){
+    alert("delte funk")
+    this.myProductService.DeleteProduct(this.Productname2).subscribe(
+      (res)=>{
+        if(res){
+          this.getAllProducts();
+        }
+        this.ResponsMassage=(res)?"deleteWork":"delete faild";
+        alert(this.ResponsMassage)
+      }
+    )
+  }
+
+ 
+  AddNewBook():void{
+    let NewProduct:Product={ProductName:"arci",Price:20};
+    alert(NewProduct.Price)
+    let callback=(bool:boolean)=>{this.DidPostWork=(bool)?"postWorks":"postdontwork";}
+    this.myProductService.AddNewProduct(NewProduct,callback)
+  }
+
+  EditProduct():void{
+    let NewProduct:Product={ProductName:"arci",Price:30};
+    alert(NewProduct.Price)
+    let callback=(bool:boolean)=>{this.DidPutWork=(bool)?"postWorks":"postdontwork";}
+    this.myProductService.EditProduct(NewProduct.ProductName,NewProduct,callback)
+  }
+  
+
 
   constructor(private myProductService:ProductService){
-    this.productList=myProductService.productList;
+  }
+  
+  ngOnInit(){
+    this.productList=this.myProductService.productList;
+    
   }
 
 }
--- src/app/shared/services/Productservice.service.ts
+++ src/app/shared/services/Productservice.service.ts
@@ -2,6 +2,7 @@ import {Product} from "../models/product.modle"
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { ProductListclass } from "../models/productList.modle";
+import { Observable } from "rxjs/Observable";
 
 
 @Injectable()
@@ -9,15 +10,39 @@ export class ProductService{
 
     productList:ProductListclass=new ProductListclass()
 
+    
+
 
     constructor(private myHttpClient:HttpClient){
-        this.InitProducts();
-        console.log(this.productList.productsArry[0].ProductName)
-       
     }
 
     InitProducts():void{
+        alert("in the funck")
         let url:string="http://localhost:52786/api/Prosucts";
-        this.myHttpClient.get(url).subscribe((x:Array<Product>)=>{this.productList.productsArry=x;});
+        this.myHttpClient.get(url).subscribe((x:Array<Product>)=>{this.productList.productsArry=x;}
+        )   
     }
+    SetSpecificProduct(productname:string):void{
+        alert("in the secend functin")
+        let url=`http://localhost:52786/api/Prosucts?productname=${productname}`
+        this.myHttpClient.get(url).subscribe((x:Product)=>{this.productList.singleProduct=x;console.log(this.productList.singleProduct)});
+
+    }
+
+    DeleteProduct(productname:string):Observable<boolean>{
+        alert("service delte funk")
+        let url:string=`http://localhost:52786/api/Prosucts?productname=${productname}`;
+        return this.myHttpClient.delete<boolean>(url);
+    }
+
+    AddNewProduct(Productparam:Product,callback:(bool:boolean)=>void):void{
+        alert(JSON.stringify(Productparam))
+        this.myHttpClient.post<boolean>("http://localhost:52786/api/Prosucts",JSON.stringify(Productparam),{headers:{"content-type":"application/json"}}).subscribe(()=>{this.InitProducts();callback(true);},()=>{callback(false)});
+    }
+
+    EditProduct(productname:string,Productparam:Product,callback:(bool:boolean)=>void):void{
+        alert(JSON.stringify(Productparam))
+        this.myHttpClient.put<boolean>(`http://localhost:52786/api/Prosucts?productname=${productname}`,JSON.stringify(Productparam),{headers:{"content-type":"application/json"}}).subscribe(()=>{this.InitProducts();callback(true);},()=>{callback(false)});
+    }
+
 }
